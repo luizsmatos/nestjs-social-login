902364f1c1efc6f19e7bdf51c3b4301dcbbaba1f
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-01 11:25:09 -0300

    feat(auth-github.module.ts): add AuthValidateModule import and AuthGitHubController to improve authentication functionality

--- src/auth/github/auth-github.module.ts
+++ src/auth/github/auth-github.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { GitHubStrategy } from './strategies/github.strategy';
+import { AuthValidateModule } from '../auth-validate/auth-validate.module';
+import { AuthGitHubController } from './auth-github.controller';
 
 @Module({
-  imports: [],
+  imports: [AuthValidateModule],
+  controllers: [AuthGitHubController],
   providers: [GitHubStrategy],
 })
 export class AuthGitHubModule {}
