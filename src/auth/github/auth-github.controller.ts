42bd4e9cf0f7f9f2ccb6e5cc06e8da6045d5c974
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-01 11:24:56 -0300

    feat(auth-github): add AuthValidateService to validate user login in AuthGitHubController

--- src/auth/github/auth-github.controller.ts
+++ src/auth/github/auth-github.controller.ts
@@ -2,14 +2,21 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { GitHubAuthGuard } from './guards/github-auth.guard';
 import { Request } from 'express';
 
+import { AuthValidateService } from '../auth-validate/services/auth-validate.service';
+
 @UseGuards(GitHubAuthGuard)
 @Controller('auth/github')
 export class AuthGitHubController {
+  constructor(private readonly authValidateService: AuthValidateService) {}
+
   @Get('/login')
   async login() {}
 
   @Get('/callback')
   async callback(@Req() request: Request) {
-    return request.user;
+    const user = request.user as any;
+    const validate = await this.authValidateService.validateLogin(user);
+
+    return { accessToken: validate.accessToken };
   }
 }
